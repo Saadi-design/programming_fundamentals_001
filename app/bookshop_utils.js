b4c594795e4002edada5848af71982551f457f44
Saadiya Moid <saadiyamoid@gmail.com>
2019-10-08 20:25:46 +0100

    please check failed test on line 31

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -1,7 +1,19 @@
 function addVAT(price) {
+
     return price + (0.2 * price);
+};
+
+function getFullName(a,b) {
+    const fullName =("Haruki " + "Murakami")
+    return fullName 
+}
+function makeHalfPrice(price) {
+    return price - (0.5 * price)
 }
 
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice
 };
+
--- test/utils.test.js
+++ test/utils.test.js
@@ -8,4 +8,26 @@ describe("utils.addVAT", () => {
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
+
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
+});
+
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+});
+
+describe("utils.makeHalfPrice", () => {
+  test("returns 50 when passed 100", () => {
+    expect(utils.makeHalfPrice(100)).toBe(50);
+  });
+});
+
+describe("utils.countBooks", () => {
+  test("returns the total number of books in the array", () => {
+    expect(utils.countBooks(["Far from the Maddening crowd", "Gone with the Wind"])).toBe(2);
+  });
 });
